3dcc740fd254dc491cfb7b66bc143935cd195af8
azaazazaa <vitruviy2100@gmail.com>
2022-04-06 00:12:40 +0300

    set main base for latex template

--- PythonCode/example.py
+++ PythonCode/example.py
@@ -1,3 +1,10 @@
-a = 2
-b = 5
-print(a+b)
+import matplotlib.pyplot as plt
+import os
+
+fig_dir = os.path.join(os.getcwd(), '..', 'src', 'figures')
+
+fig = plt.figure(figsize=(3,3))
+plt.plot([2, 3, 5], [1, 2, 3])
+plt.tight_layout()
+
+plt.savefig(os.path.join(fig_dir, 'example.png'))
